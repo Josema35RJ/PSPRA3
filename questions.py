11bd6d553cf0f3bcf5cc8e0407372a2053cd2743
José Manuel Reyes Jiménez <reyes.jijos20@cadiz.salesianos.edu>
2024-01-16 17:21:13 +0100

    update start

---
+++ questions.py
@@ -0,0 +1,226 @@
+questions = [
+    {
+        'question': '¿Cuál es la capital de España?',
+        'options': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es la moneda oficial de Japón?',
+        'options': ['Yen', 'Dólar', 'Euro', 'Libra'],
+        'answer': 0
+    },
+    {
+        'question': '¿Quién escribió "Don Quijote de la Mancha"?',
+        'options': ['Miguel de Cervantes', 'Gabriel García Márquez', 'Ernest Hemingway', 'William Shakespeare'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el río más largo del mundo?',
+        'options': ['Nilo', 'Amazonas', 'Yangtsé', 'Misisipi'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el planeta más grande del sistema solar?',
+        'options': ['Tierra', 'Marte', 'Júpiter', 'Venus'],
+        'answer': 2
+    },
+    {
+        'question': '¿Cuál es el animal más rápido del mundo?',
+        'options': ['Guepardo', 'Águila real', 'Pez vela', 'Liebre'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el edificio más alto del mundo?',
+        'options': ['Burj Khalifa', 'Torre Eiffel', 'Empire State', 'Torre de Pisa'],
+        'answer': 0
+    },
+    {
+        'question': '¿Quién pintó la Mona Lisa?',
+        'options': ['Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Claude Monet'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el océano más grande del mundo?',
+        'options': ['Atlántico', 'Índico', 'Ártico', 'Pacífico'],
+        'answer': 3
+    },
+    {
+        'question': '¿Cuál es el país más poblado del mundo?',
+        'options': ['India', 'Estados Unidos', 'China', 'Rusia'],
+        'answer': 2
+    },
+    {
+        'question': '¿Cuál es el idioma más hablado del mundo?',
+        'options': ['Inglés', 'Mandarín', 'Español', 'Hindi'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el planeta más cercano al sol?',
+        'options': ['Venus', 'Marte', 'Mercurio', 'Tierra'],
+        'answer': 2
+    },
+    {
+        'question': '¿Quién es el autor de "Cien años de soledad"?',
+        'options': ['Gabriel García Márquez', 'Mario Vargas Llosa', 'Julio Cortázar', 'Jorge Luis Borges'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el elemento más abundante en la atmósfera de la Tierra?',
+        'options': ['Oxígeno', 'Nitrógeno', 'Dióxido de carbono', 'Hidrógeno'],
+        'answer': 1
+    },
+    {
+        'question': '¿Quién es el autor de "La Odisea"?',
+        'options': ['Homero', 'Virgilio', 'Sófocles', 'Esquilo'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el continente más grande del mundo?',
+        'options': ['África', 'América', 'Asia', 'Europa'],
+        'answer': 2
+    },
+    {
+        'question': '¿Cuál es el país con más idiomas oficiales?',
+        'options': ['India', 'Sudáfrica', 'Canadá', 'Suiza'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el río más largo de Europa?',
+        'options': ['Ródano', 'Danubio', 'Volga', 'Elba'],
+        'answer': 2
+    },
+    {
+        'question': '¿Quién descubrió la penicilina?',
+        'options': ['Marie Curie', 'Albert Einstein', 'Alexander Fleming', 'Isaac Newton'],
+        'answer': 2
+    },
+    {
+        'question': '¿Cuál es el país más pequeño del mundo?',
+        'options': ['Mónaco', 'Vaticano', 'San Marino', 'Liechtenstein'],
+        'answer': 1
+    },
+    {
+        'question': '¿Quién es el autor de "Crimen y castigo"?',
+        'options': ['León Tolstói', 'Fyodor Dostoevsky', 'Anton Chekhov', 'Nikolai Gogol'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el país con la mayor cantidad de volcanes activos?',
+        'options': ['Japón', 'Indonesia', 'Italia', 'Estados Unidos'],
+        'answer': 1
+    },
+    {
+        'question': '¿Quién es el autor de "Moby Dick"?',
+        'options': ['Herman Melville', 'Mark Twain', 'Ernest Hemingway', 'F. Scott Fitzgerald'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el lago más profundo del mundo?',
+        'options': ['Lago Baikal', 'Lago Tanganica', 'Lago Superior', 'Lago Victoria'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el desierto más grande del mundo?',
+        'options': ['Sahara', 'Gobi', 'Kalahari', 'Antártida'],
+        'answer': 3
+    },
+    {
+        'question': '¿Cuál es el país más grande del mundo por área geográfica?',
+        'options': ['Canadá', 'China', 'Rusia', 'Estados Unidos'],
+        'answer': 2
+    },
+    {
+        'question': '¿Quién es el autor de "1984"?',
+        'options': ['George Orwell', 'Aldous Huxley', 'Ray Bradbury', 'Isaac Asimov'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el océano más pequeño del mundo?',
+        'options': ['Atlántico', 'Índico', 'Ártico', 'Pacífico'],
+        'answer': 2
+    },
+    {
+        'question': '¿Quién es el autor de "En busca del tiempo perdido"?',
+        'options': ['Marcel Proust', 'James Joyce', 'Virginia Woolf', 'Franz Kafka'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es el país con la mayor biodiversidad del mundo?',
+        'options': ['Brasil', 'Colombia', 'Australia', 'India'],
+        'answer': 1
+    },
+    {
+        'question': '¿Quién es el autor de "Guerra y paz"?',
+        'options': ['León Tolstói', 'Fyodor Dostoevsky', 'Anton Chekhov', 'Nikolai Gogol'],
+        'answer': 0
+    },
+    {
+        'question': '¿Cuál es la montaña más alta del mundo?',
+        'options': ['K2', 'Everest', 'Kilimanjaro', 'Aconcagua'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el país con más patrimonios de la humanidad?',
+        'options': ['España', 'Italia', 'China', 'Francia'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el río más largo de América?',
+        'options': ['Mississippi', 'Amazonas', 'Paraná', 'Yukón'],
+        'answer': 1
+    },
+    {
+        'question': '¿Quién descubrió la teoría de la relatividad?',
+        'options': ['Marie Curie', 'Albert Einstein', 'Alexander Fleming', 'Isaac Newton'],
+        'answer': 1
+    },
+    {
+        'question': '¿Cuál es el país más pequeño de América?',
+        'options': ['El Salvador', 'San Marino', 'Grenada', 'San Cristóbal y Nieves'],
+        'answer': 3
+    },
+    {
+        'question': '¿Quién es el autor de "Los miserables"?',
+        'options': ['Victor Hugo', 'Charles Dickens', 'Jane Austen', 'Mark Twain'],
+        'answer': 0
+    },
+    { 'question': '¿Cuál es el país con más lagos?', 
+    'options': ['Canadá', 'Rusia', 'Estados Unidos', 'Finlandia'],
+     'answer': 0 
+    },
+    { 'question': '¿Quién escribió "Ulises"?', 
+    'options': ['James Joyce', 'Ernest Hemingway', 'Virginia Woolf', 'F. Scott Fitzgerald'],
+     'answer': 0 
+    },
+    { 'question': '¿Cuál es la ciudad más poblada del mundo?', 
+    'options': ['Shanghai', 'Tokio', 'Delhi', 'Sao Paulo'],
+     'answer': 1 
+    },
+    { 'question': '¿Cuál es el país con más ríos?', 
+    'options': ['Brasil', 'Rusia', 'Canadá', 'India'],
+     'answer': 2 
+    },
+    { 'question': '¿Cuál es el río más largo de Asia?',
+     'options': ['Ganges', 'Mekong', 'Yangtsé', 'Indo'],
+      'answer': 2 
+    },
+    { 'question': '¿Quién descubrió la estructura del ADN?',
+     'options': ['Watson y Crick', 'Einstein', 'Darwin', 'Newton'],
+      'answer': 0 },
+    { 'question': '¿Cuál es el país más pequeño de Asia?',
+     'options': ['Maldivas', 'Singapur', 'Bhután', 'Líbano'],
+      'answer': 0 
+    },
+    { 'question': '¿Quién es el autor de "Madame Bovary"?',
+     'options': ['Gustave Flaubert', 'Victor Hugo', 'Charles Dickens', 'Leo Tolstoy'],
+      'answer': 0 
+    },
+    { 'question': '¿Cuál es el océano más salado?',
+     'options': ['Atlántico', 'Índico', 'Ártico', 'Pacífico'],
+      'answer': 1 
+    },
+    { 'question': '¿Cuál es el país con más montañas?',
+     'options': ['China', 'India', 'Nepal', 'Argentina'],
+      'answer': 0 
+    },
+]
