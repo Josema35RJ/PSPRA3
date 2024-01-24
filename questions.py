cbd1d050a4a4870b29691b6eb69102d127e23727
Ivan Cruzado <cruzado.peiva21@cadiz.salesianos.edu>
2024-01-24 18:04:55 +0100

    commit question

--- questions.py
+++ questions.py
@@ -2,102 +2,102 @@ questions = [
     {
         'question': '¿Cuál es la capital de España?',
         'options': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla'],
-        'answer':'a'
+        'answer':'1'
     },
     {
         'question': '¿Cuál es la moneda oficial de Japón?',
         'options': ['Yen', 'Dólar', 'Euro', 'Libra'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Quién escribió "Don Quijote de la Mancha"?',
         'options': ['Miguel de Cervantes', 'Gabriel García Márquez', 'Ernest Hemingway', 'William Shakespeare'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Cuál es el río más largo del mundo?',
         'options': ['Nilo', 'Amazonas', 'Yangtsé', 'Misisipi'],
-        'answer': 'b'
+        'answer': '2'
     },
     {
         'question': '¿Cuál es el planeta más grande del sistema solar?',
         'options': ['Tierra', 'Marte', 'Júpiter', 'Venus'],
-        'answer': 'c'
+        'answer': '3'
     },
     {
         'question': '¿Cuál es el animal más rápido del mundo?',
         'options': ['Guepardo', 'Águila real', 'Pez vela', 'Liebre'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Cuál es el edificio más alto del mundo?',
         'options': ['Burj Khalifa', 'Torre Eiffel', 'Empire State', 'Torre de Pisa'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Quién pintó la Mona Lisa?',
         'options': ['Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Claude Monet'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Cuál es el océano más grande del mundo?',
         'options': ['Atlántico', 'Índico', 'Ártico', 'Pacífico'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Cuál es el país más poblado del mundo?',
         'options': ['India', 'Estados Unidos', 'China', 'Rusia'],
-        'answer': 'c'
+        'answer': '3'
     },
     {
         'question': '¿Cuál es el idioma más hablado del mundo?',
         'options': ['Inglés', 'Mandarín', 'Español', 'Hindi'],
-        'answer': 'b'
+        'answer': '2'
     },
     {
         'question': '¿Cuál es el planeta más cercano al sol?',
         'options': ['Venus', 'Marte', 'Mercurio', 'Tierra'],
-        'answer': 'c'
+        'answer': '3'
     },
     {
         'question': '¿Quién es el autor de "Cien años de soledad"?',
         'options': ['Gabriel García Márquez', 'Mario Vargas Llosa', 'Julio Cortázar', 'Jorge Luis Borges'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Cuál es el elemento más abundante en la atmósfera de la Tierra?',
         'options': ['Oxígeno', 'Nitrógeno', 'Dióxido de carbono', 'Hidrógeno'],
-        'answer': 'b'
+        'answer': '2'
     },
     {
         'question': '¿Quién es el autor de "La Odisea"?',
         'options': ['Homero', 'Virgilio', 'Sófocles', 'Esquilo'],
-        'answer': 'a'
+        'answer': '1'
     },
     {
         'question': '¿Cuál es el continente más grande del mundo?',
         'options': ['África', 'América', 'Asia', 'Europa'],
-        'answer': 'c'
+        'answer': '2'
     },
     {
         'question': '¿Cuál es el país con más idiomas oficiales?',
         'options': ['India', 'Sudáfrica', 'Canadá', 'Suiza'],
-        'answer': 'b'
+        'answer': '2'
     },
     {
         'question': '¿Cuál es el río más largo de Europa?',
         'options': ['Ródano', 'Danubio', 'Volga', 'Elba'],
-        'answer': 'c'
+        'answer': '3'
     },
     {
         'question': '¿Quién descubrió la penicilina?',
         'options': ['Marie Curie', 'Albert Einstein', 'Alexander Fleming', 'Isaac Newton'],
-        'answer': 'c'
+        'answer': '3'
     },
     {
         'question': '¿Cuál es el país más pequeño del mundo?',
         'options': ['Mónaco', 'Vaticano', 'San Marino', 'Liechtenstein'],
-        'answer': 'b'
+        'answer': '2'
     },
    
 ]
